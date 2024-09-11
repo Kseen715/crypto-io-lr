ceba23b58f4d07d90c0dfe29f62707f15016addf
kseen <korenevdn@gmail.com>
2024-09-11 16:58:11 +0300

    Split tests

---
+++ tests/win_test_all.ps1
@@ -0,0 +1,5 @@
+$ .\tests\win_test_png.ps1
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+$ .\tests\win_test_mp4.ps1
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
--- tests/win_test_png.ps1
+++ tests/win_test_png.ps1
@@ -16,19 +16,3 @@ if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 & .\tests\win_compare_files.ps1 .\tests\kokkoro-astrum.png .\temp\demo-png-dec.png
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-
-Write-Host "<] Test on mp4 file"
-& $build_path -m keygen -g .\temp\key.bin
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-& $build_path -m iv -g .\temp\iv.bin
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-& $build_path -m enc -k .\temp\key.bin -i .\temp\iv.bin -f  ".\tests\nc-video.mp4" -c "no" -o ".\temp\demo-mp4.enc"
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-& $build_path -m dec -k .\temp\key.bin -i .\temp\iv.bin -f ".\temp\demo-mp4.enc" -c "no" -o ".\temp\demo-mp4-dec.mp4"
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-& .\tests\win_compare_files.ps1 .\tests\nc-video.mp4 .\temp\demo-mp4-dec.mp4
-if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
